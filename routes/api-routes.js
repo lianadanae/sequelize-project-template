efa2a00129e7fb1729a3833ee0de2ed54dcdb858
lianadanae <lianabette@gmail.com>
2019-11-09 13:32:01 -0500

    api-routes updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,25 +12,42 @@ const db = require('../models');
 module.exports = function(app) {
   // GET route for getting all of the todos
   app.get('/api/todos', async (req, res) => {
-
+    const results = await db.Todo.findAll();
+    res.json(results);
   });
 
   // POST route for saving a new todo.
   // We can create a todo using the data on req.body
   app.post('/api/todos', async (req, res) => {
-
+    const {text} = req.body;
+    const result = await db.Todo.create({
+      text,
+      complete: false,
+    });
+    res.send(result);
   });
 
   // DELETE route for deleting todos.
   // We can access the ID of the todo to delete in
   // req.params.id
   app.delete('/api/todos/:id', async (req, res) => {
-
+    const result = await db.Todo.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).send(result);
   });
 
   // PUT route for updating todos.
   // We can access the updated todo in req.body
   app.put('/api/todos', async (req, res) => {
-
+    const {id, text, complete} = req.body;
+    const result = await db.Todo.update(
+        {text, complete},
+        {
+          where: {id},
+        });
+    res.send(result);
   });
 };
